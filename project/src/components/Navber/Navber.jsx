bd4f34514493fc034ab0191d6a02146d026fa671
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-02-22 13:55:23 +0600

    navber open close done

--- project/src/components/Navber/Navber.jsx
+++ project/src/components/Navber/Navber.jsx
@@ -1,6 +1,10 @@
-
+import { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import { ImCross } from "react-icons/im";
 
 const Navber = () => {
+
+    const [open, setOpen]=useState();
 
     const routes = [
         { id: 1, path: "/", name: "Home" },
@@ -8,16 +12,26 @@ const Navber = () => {
         { id: 3, path: "/services", name: "Services" },
         { id: 4, path: "/contact", name: "Contact" },
         { id: 5, path: "/dashboard", name: "Dashboard" }
-      ];
+    ];
 
 
 
     return (
         <div>
+            <div className="md:hidden text-2xl" onClick={()=>setOpen(!open)}>
+
+                {
+                    open === true ?<ImCross />:<IoMdMenu />
+                }
+
+                
+            </div>
+
+
             <ul className="md:flex mb-5">
-            {
-                routes.map(route=> <li key={route.id} className="mr-2"><a  href={route.path}></a>{route.name}</li>)
-            }
+                {
+                    routes.map(route => <li key={route.id} className="mr-2"><a href={route.path}></a>{route.name}</li>)
+                }
             </ul>
         </div>
     );
