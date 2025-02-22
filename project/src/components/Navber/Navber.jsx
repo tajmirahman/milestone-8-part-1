2ba76b950c93483200de197a911f5d77e06c049e
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-02-22 14:31:21 +0600

    menu show with open and close click

--- project/src/components/Navber/Navber.jsx
+++ project/src/components/Navber/Navber.jsx
@@ -4,7 +4,7 @@ import { ImCross } from "react-icons/im";
 
 const Navber = () => {
 
-    const [open, setOpen]=useState();
+    const [open, setOpen] = useState();
 
     const routes = [
         { id: 1, path: "/", name: "Home" },
@@ -18,19 +18,21 @@ const Navber = () => {
 
     return (
         <div>
-            <div className="md:hidden text-2xl" onClick={()=>setOpen(!open)}>
+            <div className="md:hidden text-2xl" onClick={() => setOpen(!open)}>
 
                 {
-                    open === true ?<ImCross />:<IoMdMenu />
+                    open === true ? <ImCross className="text-red-600"/> : <IoMdMenu />
                 }
 
-                
+
             </div>
 
 
-            <ul className="md:flex mb-5">
+            <ul className={`${open ? 'top-6':'-top-60'} md:flex mb-5 px-10 duration-1000 absolute`}>
                 {
-                    routes.map(route => <li key={route.id} className="mr-2"><a href={route.path}></a>{route.name}</li>)
+                    routes.map(route => <li key={route.id} className="mr-2">
+                        <a href={route.path}>{route.name}</a>
+                    </li>)
                 }
             </ul>
         </div>
